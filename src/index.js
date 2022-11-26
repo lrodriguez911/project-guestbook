14731cb61cdbecbf91ff4b71f792b7e4d03a4e93
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-11-25 21:10:39 -0300

    first remote commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express  = require('express')
+const path  = require('path')
+const morgan  = require('morgan');
+const { urlencoded } = require('express');
+
+
+//Initializations
+const app = express()
+
+//Settings
+const port = process.env.PORT || 3000
+app.set('port', port);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(morgan('dev'))
+app.use(express.urlencoded({extends: false}))
+
+
+//Routes
+app.use(require('./routes/entries.routes'));
+
+
+
+// 404 handler
+app.use((req, res) =>{
+    res.status(404).send('404 not found')
+})
+
+
+//Starting the app
+
+app.listen(app.get(port), () => {
+    console.log(`Server is listen on port ${port}`);
+})
